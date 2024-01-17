0a67f79a16778e00e11a417a6fb7d41c0bb8ffaa
Vittorio Giordano <56154360+vittoriogiordano@users.noreply.github.com>
2024-01-17 10:53:10 +0100

    Aggiunte Servlet HomeServlet2, LoginAmministratoreServlet, LoginServlet e LogoutServlet

---
+++ GOS/src/main/java/Servlet/LogoutServlet.java
@@ -0,0 +1,26 @@
+package Servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/Logout")
+public class LogoutServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if(request.getSession().getAttribute("utente")!= null)
+            request.getSession().removeAttribute("utente");
+        else
+            request.getSession().removeAttribute("amministratore");
+
+        RequestDispatcher requestDispatcher= request.getRequestDispatcher("WEB-INF/jsp/index.jsp");
+        requestDispatcher.forward(request,response);
+    }
+}
